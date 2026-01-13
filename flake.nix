2dd32b7a4038c1c679f165333dfce59ce84ff19c
qmi <minh.pham1126@gmail.com>
2026-01-13 07:29:22 +0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    ,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python313Packages.pygame
+          ];
+
+          shellHook = ''
+            echo "Installed pygame"
+          '';
+        };
+      }
+    );
+}
